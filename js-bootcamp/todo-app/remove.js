1fa94db5ccab6615ec9da44d6438bee9553597d7
Jesus Otero <jesusotero@Jesuss-MacBook-Pro.local>
2019-09-24 17:24:45 +0200

    primer cambio

--- js-bootcamp/todo-app/remove.js
+++ js-bootcamp/todo-app/remove.js
@@ -1,7 +1,31 @@
-const ps = document.querySelectorAll('p')
-ps.forEach(function(p) {
-    if (p.textContent.includes('p1')) { debugger
-        p.remove()}
-   
-//else return p
-});
+const todos=[{
+    text: 'Order car food',
+    completed: false},
+    {text: 'Clean Kitchen',
+    completed: true},
+    {text: 'Buy food',
+    completed: true},
+    {text: 'Do work',
+    completed: false},
+    {text: 'Excercise',
+    completed: true
+    }]
+    
+    // todos still to complete
+    const incompleteTodos = todos.filter(function(todos){
+        return !todos.completed
+    })
+    
+    const summary = document.createElement('p')
+    summary.textContent = `you have ${incompleteTodos.length} todos left`
+    document.querySelector('body').appendChild(summary)
+    
+    
+    
+    //First excercise remove if contains a word 
+    // const ps = document.querySelectorAll('p')
+    // ps.forEach(function(p) {
+    //     if (p.textContent.includes('p1')) { debugger
+    //         p.remove()}
+       
+    // });
