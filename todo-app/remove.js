8a50b19070441048cf63c16f448b822a97590f5c
Jesus Otero <jesusotero@Jesuss-MacBook-Pro.local>
2019-09-25 16:00:55 +0200

    not working version yet filterAll

--- todo-app/remove.js
+++ todo-app/remove.js
@@ -19,31 +19,65 @@ const todos = [{
     completed: true
 }]
 
+const filters = {
+    searchContent: ''
+}
+
+const FilterToDos = function (todos, filters) {
+    const filterALL = todos.filter(function (todo) {
+        return todo.text.ToLowercase().includes(filters.searchContent.ToLowercase())
+
+    }
+    )}
+
+    const incompleteTodos = filterAll.filter(function (todos) {
+        return !todos.completed
+    })
+
+    document.querySelector('#division1').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `you have ${incompleteTodos.length} todos left`
+    document.querySelector('#division1').appendChild(summary)
+
+
+filterAll.forEach(function (todos) {
+    const p1 = document.createElement('p')
+    p1.textContent = todos.text
+    document.querySelector('#division1').appendChild(p1)
+
+})
 //todos still to complete
-const incompleteTodos = todos.filter(function (todos) {
-    return !todos.completed
+FilterToDos(todos, filters)
+
+
+
+
+
+document.querySelector('#FilterText').addEventListener('input', function (e) {
+    filters.searchContent = e.target.value
+    FilterToDos(todos, filters)
 })
 
-const summary = document.createElement('h2')
-summary.textContent = `you have ${incompleteTodos.length} todos left`
-document.querySelector('body').appendChild(summary)
 
-todos.forEach(function (todos) {
-    const p = document.createElement('p')
-    p.textContent = todos.text
-    document.querySelector('body').appendChild(p)
+// todos.forEach(function (todos) {
+//     const p = document.createElement('p')
+//     p.textContent = todos.text
+//     document.querySelector('#division1').appendChild(p)
 
-})
+// })
 
 document.querySelector('#AddTodo').addEventListener('click', function (e) {
     console.log('buen trabajo perrita')
     console.log(e)
 })
 
-document.querySelector('#UserTodo').addEventListener('input',function(e){
-console.log(e.target.value) 
+document.querySelector('#UserTodo').addEventListener('input', function (e) {
+    console.log(e.target.value)
 
 })
+
+
 
 //First excercise remove if contains a word 
 // const ps = document.querySelectorAll('p')
