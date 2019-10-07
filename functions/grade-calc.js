42e1922c8dac9a53ceb74ddf0a7adb719b683af9
Jesus Otero <jesusotero@Jesuss-MacBook-Pro.local>
2019-10-07 19:58:21 +0200

    Try and Catch

--- functions/grade-calc.js
+++ functions/grade-calc.js
@@ -1,26 +1,35 @@
 
 
-let testScore= function  (studentsScore){
+let testScore = function (studentsScore) {
+if ( typeof studentsScore === 'number') {
+   
 
-if ( studentsScore >= 90 && studentsScore <=100){
+    if (studentsScore >= 90 && studentsScore <= 100) {
 
-    PosibleScore = 'your score is A'
-} else if ( studentsScore >=  80 && studentsScore <=89){
+        PosibleScore = 'your score is A'
+    } else if (studentsScore >= 80 && studentsScore <= 89) {
 
-    PosibleScore = 'your score is B'
-} 
-else if ( studentsScore >=  79 && studentsScore <=70){
+        PosibleScore = 'your score is B'
+    }
+    else if (studentsScore >= 79 && studentsScore <= 70) {
 
-    PosibleScore = 'your score is C'
-} 
-else if ( studentsScore >=  60 && studentsScore <=69){
+        PosibleScore = 'your score is C'
+    }
+    else if (studentsScore >= 60 && studentsScore <= 69) {
 
-    PosibleScore = 'your score is D'
-} else{
+        PosibleScore = 'your score is D'
+    } else {
 
-    PosibleScore = 'your score is E'
-} 
+        PosibleScore = 'your score is E'
+    }
     return `your grade is ${PosibleScore} (${studentsScore}%)`
 }
+else{ throw Error('Argument not Allowed')}
+}
 
-console.log(testScore(85))
+
+// console.log(testScore(32))
+try {
+    console.log(testScore(32))
+    }
+catch (e) { console.log(e.message) }
