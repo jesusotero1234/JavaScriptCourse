b7e97c79a32df00499316f52490b47272b31fa3f
Jesus Otero <jesusotero@Jesuss-MacBook-Pro.local>
2019-10-14 22:47:40 +0200

    Using FETCH and location to get country details

--- oop/request.js
+++ oop/request.js
@@ -10,3 +10,11 @@ const getCountryDetails = (word) => {
     )
 }
 const countryCode = 'MX'
+
+const getLocation = () => {
+    return fetch('http://ipinfo.io/json?token=9b4cb9a21e0244').then((response) => {
+        if (response.status === 200) {
+            return response.json()
+        } else { throw new Error('An error in the IP has occurred') }
+    })
+}
