b52da204adc057b4b5b1eb8eb5192121a78a3ff8
Jesus Otero <jesusotero@Jesuss-MacBook-Pro.local>
2019-10-14 19:39:48 +0200

    codwars object to String

--- oop/app.js
+++ oop/app.js
@@ -8,15 +8,44 @@ let RemainguessesEL = document.querySelector('#remainGuesses')
 const puzzleEL = document.querySelector('#showWord')
 // remainGuesses.textContent = test1.getPuzzle()
 puzzleEL.textContent = test1.puzzle
-RemainguessesEL.textContent= test1.statusMessage
+RemainguessesEL.textContent = test1.statusMessage
 // test1.status === 'playing'?RemainguessesEL.textContent = test1.remainGuesses :RemainguessesEL.textContent = ''
 console.log(test1.status)
 
-document.querySelector('#guessInput').addEventListener('keypress',function(e){
-const guess = String.fromCharCode(e.charCode)
-test1.makeGuess(guess)
-puzzleEL.textContent = test1.puzzle
-// test1.status === 'playing'?RemainguessesEL.textContent = test1.remainGuesses :RemainguessesEL.textContent = ''
-RemainguessesEL.textContent= test1.statusMessage
-console.log(test1.status)
+document.querySelector('#guessInput').addEventListener('keypress', function (e) {
+    const guess = String.fromCharCode(e.charCode)
+    test1.makeGuess(guess)
+    puzzleEL.textContent = test1.puzzle
+    // test1.status === 'playing'?RemainguessesEL.textContent = test1.remainGuesses :RemainguessesEL.textContent = ''
+    RemainguessesEL.textContent = test1.statusMessage
+    console.log(test1.status)
+})
+
+
+
+
+const getCountryDetails = (word, callback) => {
+    const request = new XMLHttpRequest()
+    request.addEventListener('readystatechange', (e) => {
+        if (e.target.status === 200 && e.target.readyState === 4) {
+            const data = JSON.parse(e.target.responseText)
+             const country = data.find((country) => country.alpha2Code === word)
+            callback(undefined,country)
+        } else if (e.target.readyState === 4) {
+            callback('An error has taken place',undefined)
+        }
+    })
+
+request.open('GET', 'http://restcountries.eu/rest/v2/all')
+request.send()
+}
+
+const countryCode = 'MX'
+
+getCountryDetails(countryCode, (error, country) =>{
+if (error){
+    console.log(`Error: ${error}`)
+}
+else{console.log(country)}
+
 })
--- oop/codewars.js
+++ oop/codewars.js
@@ -1,103 +1,36 @@
-//Best practice to do this formula
-function accum2(s) {
-return s.split('').map((c,i)=>(c.toUpperCase() + c.toLowerCase().repeat(i))).join('-')
+function list(names) {
+  if (names.length === 1) {return console.log(names.pop().name)}
+  else if (names.length === 0){return console.log([])}
+  else{
+  // const lastEl = names.pop().name
+  
+  const test = []
+  names.forEach((name) => {
+     test.push(name.name)
+  })
+const lastEl = test.pop()
+const  newArray = test.join(', ')
+  return console.log(`${newArray} & ${lastEl}`)
+}
 }
 
-//Function that creates a new string and acumulate 
-function accum(s) {
-  const divideString = s.toLowerCase().split('')
-  let length1 = divideString.length
-  let accum = 0
-  let arrayTest = []
-  divideString.forEach((letter) => {
-    for (let i = 0; i <= divideString.length; i++) {
-      console.log(arrayTest)
-      
-        if (letter === divideString[i]) {
-         
-          let test = letter.repeat(i + 1)
-          // chequea si ya fue incluido en el array anterior
-          if (!arrayTest.includes(letter.toUpperCase())) {
-          arrayTest.push(test.replace(letter, letter.toUpperCase()))
-         console.log(!arrayTest.includes(letter.toUpperCase()))
-          return false
-        }
-      } else {
-        for ( let j = accum+1; j <= divideString.length; j++) {
-            if (letter === divideString[j]) {
-              accum += 1
-              let test = letter.repeat(j + 1)
-              arrayTest.push(test.replace(letter, letter.toUpperCase()))
-             
-              return false
-            }}}}})
 
-  return arrayTest.join('-')
+list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'},{name: 'Homer'},{name: 'Marge'}])
+list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'}])
+list([{name: 'Bart'},{name: 'Lisa'}])
+list([{name: 'Bart'}])
+
+list([])
+function list2(names) {
+  if (names.length === 1) {return console.log(names.pop().name)}
+  else if (names.length === 0){return console.log([])}
+  else{
+  const lastEl = names.pop().name
+  newArray = names
+  const test = []
+  newArray.forEach((name) => {
+     test.push(name.name)
+  })
+  return console.log(`${test.join(', ')} & ${lastEl}`)
 }
-
-console.log(accum("ZpglnRxqenU"))
-console.log(accum2("ZpglnRxqenU"))
-console.log('Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu')
-//Output should be -> "A-Bb-Ccc-Dddd"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, 
-// //the longest possible, containing distinct letters.
-
-// const longest = (s1, s2) => {
-//   let test3 = s1 + s2
-//   let test4 = test3.toLowerCase().split('')
-// // new Set sirve para dejar solo elementos unicos (genera un objeto)
-//   const uniqueSet = new Set(test3)
-//   // [... Array] sirve para generar una array
-// const backToArray = [... uniqueSet]
-// backToArray.sort(function (a, b){
-// if (a<b) {return -1}
-// else if (a>b){return 1}
-// else{return 0}
-// })
-// //otra opcion para filtrar
-// let test = test4.filter((item,index)=> test4.indexOf(item)=== index)
-// let final = backToArray.join('')
-// return final
-
-// }
-// let a = "xyaabbbccccdefww"
-// let b = "xxxxyyyyabklmopq"
-// console.log(longest(a, b))
-
-
-
-
-
-
-
-
-
-// //programa que chequea si dentro de la misma palabra hay silabas o consonantes repetidas
-// let isIsogram = function (word) {
-//   //...
-//   let test1 = word.toLowerCase().split('')
-//   let length1 = word.length
-//     return test1.some( function(word) {
-//         return test1.indexOf(word) !== test1.lastIndexOf(word)
-// })
-
-// }
-// console.log(isIsogram('prueba s'))
+}
